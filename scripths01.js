ff3b9c4295407356b5601568e722deec1454ea8c
heerasrihs-dot <heerasrihs@gmail.com>
2026-02-21 15:02:17 +0530

    Update scripths01.js

--- scripths01.js
+++ scripths01.js
@@ -1,25 +1,33 @@
 function getWeather(){
 let city=document.getElementById("city").value;
+    if(city===""){
+        document.getElementById("result").iinerHTML="please enter a city name";
+        return;
+    }
 let apiKey="d5fe0a7b4ec664761995f3a04abb30a4";
 let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    document.getElementById("loader").style.display="block";
+    document.getElementByid("result").innerHTML="";
 fetch(url)
 .then(response=>response.json())
 .then(data=>{
+    document.getElementById("loader").style.display="none";
     if(data.cod !==200){
         document.getElementById("result").innerHTML="city not found";
         return;
  }
-    let weather=data.weather[0].main;
-    let icon="🔆";
-    if(weather==="Clouds")icon="☁️";
-    if(weather==="Rain")icon="🌧️";
-    if(weather==="BrokenCloud")icon="⛈️";
+let iconCode=data.weather[0].icon;
+    let iconUrl=`https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    let description=data.weather[0].description;
+    description=description.charAt(0).toUpperrCase()+description.slice(1);
 document.getElementById("result").innerHTML=
 `<h3>${data.name}</h3>
+<img src="${iconUrl}" alt="weather icon"><br>
 Temperature:${data.main.temp}°C<br>
 weather:${data.weather[0].description}`;
 })
 .catch(error=>{
 document.getElementById("result").innerHTML="city not found!";
 });
+
 }
