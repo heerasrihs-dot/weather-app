36a5e80ccb27b77100d381d7aaedf8541405df2b
heerasrihs-dot <heerasrihs@gmail.com>
2026-02-21 15:14:25 +0530

    Update scripths01.js

--- scripths01.js
+++ scripths01.js
@@ -1,10 +1,11 @@
 function getWeather(){
 let city=document.getElementById("city").value;
+    
+let apiKey="d5fe0a7b4ec664761995f3a04abb30a4";
     if(city===""){
         document.getElementById("result").iinerHTML="please enter a city name";
         return;
     }
-let apiKey="d5fe0a7b4ec664761995f3a04abb30a4";
 let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     document.getElementById("loader").style.display="block";
     document.getElementByid("result").innerHTML="";
@@ -24,10 +25,12 @@ document.getElementById("result").innerHTML=
 `<h3>${data.name}</h3>
 <img src="${iconUrl}" alt="weather icon"><br>
 Temperature:${data.main.temp}°C<br>
-weather:${data.weather[0].description}`;
+weather:${description}`;
 })
 .catch(error=>{
+    document.getElentById("loader").style.display="none";
 document.getElementById("result").innerHTML="city not found!";
 });
 
 }
+
